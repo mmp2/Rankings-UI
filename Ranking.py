4aa8c9be15010a733ce8d4e2378ecf18ab78fc1a
mk322 <77368272+mk322@users.noreply.github.com>
2022-04-12 23:44:30 -0700

    Add files via upload

---
+++ Ranking.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+class Ranking:
+    def __init__(self, file_path):
+        self.df = pd.read_csv(file_path, header=0, index_col=0)
+
+    def get_columns(self):
+        return list(self.df.index)
+
+    def get_num_rows(self):
+        return len(self.df.columns)
+    
+    def get_all_rankings(self):
+        rows = list(self.df.index)
+        for i in range(len(rows)):
+            ordered_list = list(self.df.iloc[i].sort_values(ascending=True).index)
+        return ordered_list
+
+
+    def get_vertical_rankings(self):
+        columns = list(self.df.columns)
+        rows = list(self.df.index)
+        ret_list = [ [] for _ in range(len(columns)) ]
+        for i in range(len(rows)):
+            ordered_list = list(self.df.iloc[i].sort_values(ascending=True).index)
+            for j in range(len(ordered_list)):
+                ret_list[j].append(ordered_list[j])
+        return ret_list
