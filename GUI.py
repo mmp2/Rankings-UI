117637098e266021db421e8169cf40ef021f7765
mk322 <77368272+mk322@users.noreply.github.com>
2022-05-25 00:33:53 -0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -421,7 +421,7 @@ class GUI:
 
 
     def set_up(self):
-        self.canvas.bind("<Button-3>", self.do_popup)
+        self.canvas.bind("<Button-2>", self.do_popup)
         self.canvas.bind("<ButtonRelease-1>",self.ret_colors)
         self.canvas.bind('<Double-1>', self.swap_left) 
         self.canvas.bind('<ButtonPress-1>', self.selectItem)
