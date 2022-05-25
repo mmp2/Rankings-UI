639f52391cbe9c123f22e156e883947c8b2814fc
mk322 <77368272+mk322@users.noreply.github.com>
2022-05-24 23:52:24 -0700

    Delete Ratings.py

--- Ratings.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-class ratings:
-    def __init__(self, file_path) -> None:
-        self.df = pd.read_csv(file_path, header=0, index_col=0)
-        
-    def get_rating(self, reviewer, proposal):
-        return self.df.loc[reviewer][proposal]
