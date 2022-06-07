2a9db0e5b9a605d614cb721e8f1a0378554b8748
mk322 <77368272+mk322@users.noreply.github.com>
2022-06-07 00:21:18 -0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -441,7 +441,7 @@ class GUI:
 
 
     def set_up(self):
-        self.canvas.bind("<Button-3>", self.do_popup)
+        self.canvas.bind("<Button-2>", self.do_popup)
         self.canvas.bind("<ButtonRelease-1>",self.ret_colors)
         self.canvas.bind('<Double-1>', self.swap_left) 
         self.canvas.bind('<ButtonPress-1>', self.selectItem)
@@ -467,75 +467,3 @@ class GUI:
     def show(self):
         self.legend_sub()
         self.root.mainloop()
-
-    class change_window:
-        def __init__(self, dict, gui):
-            self.gui = gui
-            self.root2 = Tk()
-            self.root2.title('Legend Window for Rankings UI')
-            self.root2.geometry('700x500+100+150')
-            
-            # Create the list of questions
-            # Pass them into an option menu
-            # Create an entry for the answer
-            # Create submit button
-            self.dict = dict
-            self.graph_attr = list(dict.keys())
-            self.ratings = list(dict.values())
-            self.var_list = []
-            
-            for i in range(len(self.graph_attr)):
-                tkvarq = StringVar(self.root2)
-                tkvarq.set("Please Select a Rating")
-                self.var_list.append(tkvarq)
-                self.create_wigets(self.graph_attr[i], i)
-
-            submit_button = Button(self.root2, text="Submit and Reopen the Main Window", command=self.return_pairs)
-            paddings = {'padx': 5, 'pady': 5}
-            submit_button.grid(column=1, row=len(self.graph_attr))
-            #Answer entry
-            #answer_entry = Entry(root, width=30)
-            #answer_entry.pack()
-
-        
-        def create_wigets(self, ga, order):
-            # padding for widgets using the grid layout
-            paddings = {'padx': 5, 'pady': 5}
-
-            # label
-            label = ttk.Label(self.root2, text=f'Please Select the Rating Displayed by the Graphical Attribute of {ga}:')
-            label.grid(column=0, row=order, sticky=tk.W, **paddings)
-            if order != 0:
-            # option menu
-                option_menu = ttk.OptionMenu(
-                    self.root2,
-                    self.var_list[order],
-                    self.dict[ga],
-                    *self.ratings)
-            else:
-                option_menu = ttk.OptionMenu(
-                    self.root2,
-                    self.var_list[order],
-                    self.dict[ga],
-                    *[self.dict[ga]])
-
-            option_menu.grid(column=1, row=order, sticky=tk.W, **paddings)
-
-            # output label
-            self.output_label = ttk.Label(self.root2, foreground='red')
-            self.output_label.grid(column=0, row=1, sticky=tk.W, **paddings)
-
-        def return_pairs(self):
-            self.result = {}
-            if len(set(i.get() for i in self.var_list)) == len(self.var_list):
-                for i in range(len(self.var_list)):
-                    self.result[self.graph_attr[i]] = self.var_list[i].get()
-                self.gui.update_all_rects(self.result)
-            else:
-                messagebox.showerror(title="Illegal Matching", message="Please Make Sure to Assign every Rating to Only One Graphical Attribute.")        
-        
-        def show(self):
-            self.root2.mainloop()
-
-        def get_pair(self):
-            return self.result
