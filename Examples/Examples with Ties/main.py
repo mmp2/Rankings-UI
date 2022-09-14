70fd1f9598c78aec250131be0d3f24b2c5e4a63a
Murray Kang <77368272+mk322@users.noreply.github.com>
2022-09-13 22:29:49 -0700

    Add files via upload

---
+++ Examples/Examples with Ties/main.py
@@ -0,0 +1,26 @@
+from GUI import GUI
+from pre_process import distr_df, rankings
+from Proposal import Proposals
+from Ranking import Rankings
+from Review import Reviews
+from Reviewer import Reviewers
+
+RANKING_PATH = "ReviewerSubmissionComparisons.txt"
+RATINGS_PATH = "dummy_rating.xls"
+
+
+ratings, props, reviewers, reivews = distr_df(RATINGS_PATH, 15)
+res, tie = rankings(RANKING_PATH, ratings)
+ranks = Rankings(ratings, res, tie)
+reviewers = Reviewers(reviewers)
+reviews = Reviews(reivews)
+props = Proposals(props)
+
+
+
+def main():
+    instance = GUI(ranks, reviews, reviews, props)
+    instance.show()
+
+if __name__ == "__main__":
+    main()
