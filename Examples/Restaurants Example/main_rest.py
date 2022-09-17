e3757e5ecba3adf76dc26ae6111ab14ebfc593aa
Murray Kang <77368272+mk322@users.noreply.github.com>
2022-09-17 09:34:56 -0700

    Add files via upload

--- Examples/Restaurants Example/main_rest.py
+++ Examples/Restaurants Example/main_rest.py
@@ -1,5 +1,9 @@
 import sys
-sys.path.insert(0, "rankings_UI")
+import os
+path_to_package = os.path.join(os.path.abspath(os.getcwd()), "rankings_UI")
+path_to_dir = os.path.abspath(os.getcwd())
+sys.path.append(path_to_package)
+sys.path.append(path_to_dir)
 from GUI import GUI
 from preprocess_rest import distr_df, rankings
 from Proposal import Proposals
@@ -9,14 +13,19 @@ from Reviewer import Reviewers
 from Proposal import Proposals
 
 other_paths = {
-    "OS": "OS Ratings.csv",
-    "FQ": "FQ Ratings.csv",
-    "ST": "ST Ratings.csv",
-    "WS": "WS Ratings.csv",
-    "MK": "MK Ratings.csv"
+    "OS": os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/OS Ratings.csv"),
+    "FQ": os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/FQ Ratings.csv"),
+    "ST": os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/ST Ratings.csv"),
+    "WS": os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/WS Ratings.csv"),
+    "MK": os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/MK Ratings.csv")
 }
-ratings, props, reviewers, reivews = distr_df("Ranking.csv", "OP_new.csv", other_paths, "Reviews.csv")
-res, tie = rankings("Ranking.csv")
+ranking_path = os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/Ranking.csv")
+OP_path = os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/OP_new.csv")
+reviews_path = os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/Reviews.csv")
+config_file = os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/config_rest.toml")
+
+ratings, props, reviewers, reivews = distr_df(ranking_path, OP_path, other_paths, reviews_path)
+res, tie = rankings(ranking_path)
 ranks = Rankings(ratings, res, tie, rating_names=list(ratings.columns[:6]), overall_col_name="OP")
 reviewers = Reviewers(reviewers)
 reviews = Reviews(reivews, ["One-word Rating"])
@@ -24,7 +33,7 @@ props = Proposals(props)
 
 
 def main():
-    instance = GUI(ranks, reviewers, reviews, props, "config_rest.toml")
+    instance = GUI(ranks, reviewers, reviews, props, config_file)
     instance.show()
 
 if __name__ == "__main__":
