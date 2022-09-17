83f485b8fdc1fe0deded1297117bc6d1b23293e6
Murray Kang <77368272+mk322@users.noreply.github.com>
2022-09-17 09:34:31 -0700

    Add files via upload

--- Examples/Paper Review Example/main.py
+++ Examples/Paper Review Example/main.py
@@ -1,5 +1,9 @@
 import sys
-sys.path.insert(0, "rankings_UI")
+import os
+path_to_package = os.path.join(os.path.abspath(os.getcwd()), "rankings_UI")
+path_to_dir = os.path.abspath(os.getcwd())
+sys.path.append(path_to_package)
+sys.path.append(path_to_dir)
 from GUI import GUI
 from pre_process import process_into_dataframes, get_rankings
 from Proposal import Proposals
@@ -7,8 +11,9 @@ from Ranking import Rankings
 from Review import Reviews
 from Reviewer import Reviewers
 
-RANKING_PATH = "dummy_ranking.txt"
-RATINGS_PATH = "dummy_rating.xls"
+RANKING_PATH = os.path.join(os.path.abspath(os.getcwd()), "Examples/Paper Review Example/dummy_ranking.txt")
+RATINGS_PATH = os.path.join(os.path.abspath(os.getcwd()), "Examples/Paper Review Example/dummy_rating.xls")
+config_path = os.path.join(os.path.abspath(os.getcwd()), "Examples/Paper Review Example/config.toml")
 titles = ["Summary", "Detailed Comments", "Questions for Author", "Anonymity", "Anonymity Details","Handled Previously"]
 
 ratings, props, reviewers, reivews = process_into_dataframes(RATINGS_PATH, 15)
@@ -20,7 +25,7 @@ reviews = Reviews(reivews, review_titles=titles, prop_colname="Paper Short Name"
 props = Proposals(props)
 
 def main():
-    instance = GUI(ranks, reviews, reviews, props, "config.toml")
+    instance = GUI(ranks, reviews, reviews, props, config_path)
     instance.show()
 
 if __name__ == "__main__":
