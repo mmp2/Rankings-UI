d7be2b2c8ed9170568257bceb2ed2330f4d695c7
Murray Kang <77368272+mk322@users.noreply.github.com>
2022-09-26 20:57:27 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ setup(
     name='rankings_UI',
     version='1.0',
     license='MIT',
-    author="Marina Meila",
-    author_email='email@example.com',
+    author="Haoqiang Kang",
+    author_email='haoqik@cs.washington.edu',
     packages=find_packages('rankings_UI'),
     package_dir={'': 'rankings_UI'},
     url='https://github.com/mk322/Rankings-UI',
     keywords='GUI',
     install_requires=[
+          'toml',
           'Tkinter',
           'pandas',
           'numpy'
