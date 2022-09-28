2e6bd0e81c0f37c0d66762b74788e79545246c79
Murray Kang <77368272+mk322@users.noreply.github.com>
2022-09-28 01:33:04 -0400

    Add files via upload

--- Examples/Restaurants Example/main.py
+++ Examples/Restaurants Example/main.py
@@ -1,28 +1,22 @@
-import sys
-import os
-path_to_package = os.path.join(os.path.abspath(os.getcwd()), "rankings_UI")
-path_to_dir = os.path.abspath(os.getcwd())
-sys.path.append(path_to_package)
-sys.path.append(path_to_dir)
-from GUI import GUI
 from preprocess_rest import distr_df, rankings
-from Proposal import Proposals
-from Ranking import Rankings
-from Review import Reviews
-from Reviewer import Reviewers
-from Proposal import Proposals
+from rankingTool.GUI import GUI
+from rankingTool.Proposal import Proposals
+from rankingTool.Ranking import Rankings
+from rankingTool.Review import Reviews
+from rankingTool.Reviewer import Reviewers
+from rankingTool.Proposal import Proposals
 
 other_paths = {
-    "OS": os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/OS Ratings.csv"),
-    "FQ": os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/FQ Ratings.csv"),
-    "ST": os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/ST Ratings.csv"),
-    "WS": os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/WS Ratings.csv"),
-    "MK": os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/MK Ratings.csv")
+    "OS": "Examples/Restaurants Example/OS Ratings.csv",
+    "FQ": "Examples/Restaurants Example/FQ Ratings.csv",
+    "ST": "Examples/Restaurants Example/ST Ratings.csv",
+    "WS": "Examples/Restaurants Example/WS Ratings.csv",
+    "MK":"Examples/Restaurants Example/MK Ratings.csv"
 }
-ranking_path = os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/Ranking.csv")
-OP_path = os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/OP_new.csv")
-reviews_path = os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/Reviews.csv")
-config_file = os.path.join(os.path.abspath(os.getcwd()), "Examples/Restaurants Example/config_rest.toml")
+ranking_path = "Examples/Restaurants Example/Ranking.csv"
+OP_path = "Examples/Restaurants Example/OP_new.csv"
+reviews_path ="Examples/Restaurants Example/Reviews.csv"
+config_file =  "Examples/Restaurants Example/config_rest.toml"
 
 ratings, props, reviewers, reivews = distr_df(ranking_path, OP_path, other_paths, reviews_path)
 res, tie = rankings(ranking_path)
